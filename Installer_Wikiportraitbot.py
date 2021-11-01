dd9c656a685782b65ff8e093a3fba8febe9dcd17
Daniuu <Daniuu.Wikipedia@gmail.com>
2021-11-02 00:46:11 +0100

    Add key to clean cache
    Add a key that first cleans the cache of PyInstaller before starting the compilation.

--- Installer_Wikiportraitbot.py
+++ Installer_Wikiportraitbot.py
@@ -11,5 +11,8 @@ import PyInstaller.__main__
 PyInstaller.__main__.run([
     'Wikiportret_interface.py',
     '--onefile',
-    '--console' #Console for I/O is required
+    '--console',
+    '--clean'
+    #r'--add-data <C:\Users\Student Daniel\Anaconda3\Lib\site-packages\pip\_vendor\certifi;cacert.pem>'
+    #Console for I/O is required
 ])

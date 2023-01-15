a88440d0592cdde20f1fa5bc3ea9071c8d5b1d81
Daniuu <Daniuu.Wikipedia@gmail.com>
2023-01-15 17:51:58 +0100

    Maximize application upon opening

--- Wikiportret_GUI_interface.py
+++ Wikiportret_GUI_interface.py
@@ -8,13 +8,16 @@ This is the interface that allows Wikiportret to operate through a graphical int
 import tkinter
 import Wikiportret_core as core
 import Wikiportret_GUI_aux_functions as aux
-import Wikiportret_GUI_text_NL as text
+import Wikiportret_GUI_text_NL as nlt
 
 root = tkinter.Tk()
 root.title('Wikiportret GUI')
+root.state('zoomed')  # Open the application in full-screen
 
-# Create the frames for the input section
-input_frame = aux.make_frame(root, 1, 0.1)
-input_frame.grid(column=0, row=0, columnspan=3, sticky=tkinter.N + aux.ch)
+root.columnconfigure(0, weight=1)
+root.rowconfigure(0, weight=1)
+
+window_width, window_height = root.winfo_screenwidth(), root.winfo_screenheight()
+
 
 root.mainloop()

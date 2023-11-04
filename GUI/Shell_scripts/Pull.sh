a4d6add369f88957da31c8ebe6532a35d0d36bd5
dwithoec <daniel.withoeck@ugent.be>
2023-11-04 12:03:05 +0100

    Fix sh

--- GUI/Shell_scripts/Pull.sh
+++ GUI/Shell_scripts/Pull.sh
@@ -1,4 +1,7 @@
 # Shell script to assist in pulling the Github repo into Toolforge
 
+# Navigate to proper directory
+
+
 # Pull the github repo
 git pull git@github.com:Daniuu-Wikipedia/Wikiportrait-Bot.git

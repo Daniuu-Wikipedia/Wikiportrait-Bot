a5b5092e36c701c3f923395ec6f6c0291dd9d542
Daniuu-Wikipedia <Daniuu.wikipedia@gmail.com>
2025-10-15 20:27:31 +0200

    Allow connections for multiple queries (performance)

--- GUI/Wikiportret_db_utils.py
+++ GUI/Wikiportret_db_utils.py
@@ -9,14 +9,16 @@ import toolforge
 import json
 
 
-def query_db(query, dbname, need_all=False):
+def query_db(query, dbname, need_all=False, connection=None):
     """
     Query information from the database.
     (details on arguments & output to be added)
     """
     if not isinstance(query, str):
         return None
-    connection = toolforge.toolsdb(dbname)
+    connection_passed = connection is not None
+    if connection is None:
+        connection = toolforge.toolsdb(dbname)
     with connection.cursor() as cursor:
         cursor.execute(query)
         if need_all is True:
@@ -24,34 +26,39 @@ def query_db(query, dbname, need_all=False):
         else:
             info = cursor.fetchone()
         cursor.close()
-    connection.close()
+    if connection_passed is False:  # Obviously, we want to keep our connection alive if it is provided
+        connection.close()
     return info  # Return the relevant info & process it elsewhere
 
 
-def get_user_id(username, dbname):
+def get_user_id(username, dbname, connection=None):
     data = query_db(f"SELECT `user_id` from `users` where `username`='{username}'",
                     dbname,
-                    need_all=False)  # '' are required to get a proper query
+                    need_all=False,
+                    connection=connection)  # '' are required to get a proper query
     if isinstance(data, tuple):
         return data[0]
     return None  # No valid user
 
 
-def adjust_db(query, dbname, retrieve_id=False):
+def adjust_db(query, dbname, retrieve_id=False, connection=None):
     if not isinstance(query, str):
         return None
-    connection = toolforge.toolsdb(dbname)
+    connection_passed = connection is not None
+    if connection is None:
+        connection = toolforge.toolsdb(dbname)
     with connection.cursor() as cursor:
         cursor.execute(query)
         connection.commit()
         if retrieve_id is True:
             new_row_id = cursor.lastrowid
         cursor.close()
-    connection.close()
+    if connection_passed is False:
+        connection.close()  # Keep connection alive if passed externally
     return new_row_id if retrieve_id is True else True  # Just to indicate that everything worked out fine
 
 
-def get_tokens_from_db(dbname, operator_name):
+def get_tokens_from_db(dbname, operator_name, connection=None):
     if isinstance(operator_name, str):
         query = f"""
         select * from tokens where operator_id={get_user_id(operator_name, dbname)};
@@ -62,4 +69,4 @@ def get_tokens_from_db(dbname, operator_name):
                 """
     else:
         raise TypeError('operator_name must be str or int!')
-    return json.loads(query_db(query, dbname)[1])  # Automatically
+    return json.loads(query_db(query, dbname, connection=connection)[1])  # Automatically

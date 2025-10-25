be819e02da4b9edab0a29b8ece37f1326a145540
Daniuu-Wikipedia <Daniuu.wikipedia@gmail.com>
2025-10-25 17:25:16 +0200

    Date validators updated

--- GUI/static/js/date_validator.js
+++ GUI/static/js/date_validator.js
@@ -3,6 +3,11 @@ document.addEventListener("DOMContentLoaded", function () {
   const deathDateInput = document.getElementById("i13");
   const imageDateInput = document.getElementById("i12");
 
+function validateDates() {
+
+  const birthDateStr = birthDateInput.value;
+  const deathDateStr = deathDateInput.value;
+  const imageDateStr = imageDateInput.value;
 
   birthDateInput.classList.remove("date-error");
   deathDateInput.classList.remove("date-error");
@@ -13,13 +18,20 @@ document.addEventListener("DOMContentLoaded", function () {
   const imageDate = imageDateStr ? new Date(imageDateStr) : null;
 
   // birthDate <= deathDate
-  if (birthDate > deathDate || imageDate > deathDate || birthDate > imageDate) {
+  if (birthDate > deathDate || imageDate > deathDate) {
     birthDateInput.classList.add("date-error");
-    deathDateInput.classList.add("date-error");
-    imageDateInput.classList.add("date-error");
   }
 
+  // birthDate <= imageDate
+  if (birthDate > imageDate) {
+    birthDateInput.classList.add("date-error");
+  }
 
+  // imageDate <= deathDate
+  if (imageDate && deathDate && imageDate > deathDate) {
+    imageDateInput.classList.add("date-error");
+  }
+}
   birthDateInput.addEventListener("change", validateDates);
   deathDateInput.addEventListener("change", validateDates);
   imageDateInput.addEventListener("change", validateDates);

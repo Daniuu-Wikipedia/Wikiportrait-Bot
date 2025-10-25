8f6bd7c7ce08295a64fe203797356d9a34d7c4c6
Daniuu-Wikipedia <Daniuu.wikipedia@gmail.com>
2025-10-25 17:25:16 +0200

    Date validators updated

--- GUI/static/js/date_validator.js
+++ GUI/static/js/date_validator.js
@@ -18,20 +18,26 @@ function validateDates() {
   const imageDate = imageDateStr ? new Date(imageDateStr) : null;
 
   // birthDate <= deathDate
-  if (birthDate > deathDate || imageDate > deathDate) {
-    birthDateInput.classList.add("date-error");
+  if (birthDate != null && deathDate != null && imageDate != null) {
+      if (birthDate > deathDate || imageDate > deathDate) {
+        birthDateInput.classList.add("date-error");
+      }
   }
 
   // birthDate <= imageDate
-  if (birthDate > imageDate) {
-    birthDateInput.classList.add("date-error");
+  if (birthDate != null && imageDate != null) {
+      if (birthDate > imageDate) {
+      birthDateInput.classList.add("date-error");
+    }
   }
 
-  // imageDate <= deathDate
-  if (imageDate && deathDate && imageDate > deathDate) {
-    imageDateInput.classList.add("date-error");
+  if (imageDate != null && deathDate != null) {
+      if (imageDate && deathDate && imageDate > deathDate) {
+      imageDateInput.classList.add("date-error");
+    }
   }
 }
+
   birthDateInput.addEventListener("change", validateDates);
   deathDateInput.addEventListener("change", validateDates);
   imageDateInput.addEventListener("change", validateDates);
